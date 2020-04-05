a5b8b3bcf0987a925b247fa2b5995633f287d850
ishank <ishank.web.dev@gmail.com>
2020-04-05 15:23:34 +0530

    fix: added host

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
+const host = "0.0.0.0";
 // console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, host, () => console.log(`Listening on port ${port}`));
 
 const root = require("path").join(__dirname, "client", "build");
 
