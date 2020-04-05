559e49456a50f29c34568c40ddba7ddc44d8a8be
ishank <ishank.web.dev@gmail.com>
2020-04-05 15:13:38 +0530

    fix: static gen and package changes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get("/express_backend", (req, res) => {
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+const root = require("path").join(__dirname, "client", "build");
+
+app.use(express.static(root));
+
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root });
 });
