784712f180b669957833a66a8c80408d4779c57e
ishank <ishank.web.dev@gmail.com>
2020-04-07 01:27:43 +0530

    feat: heading improved

--- client/src/components/shared/ResumeHeading.tsx
+++ client/src/components/shared/ResumeHeading.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
+const getMediaQuery = (
+  screenSize: { tablet: string; mobileM: string },
+  size: number
+) => `
+
+@media ${screenSize.tablet} {
+  font-size: calc(${size}em - 2em);
+}
+@media ${screenSize.mobileM} {
+  font-size: calc(${size}em - 3em);
+  margin-top: 15%;
+}
+`;
+
 const ResumeHeadingWrapper = styled.div`
   position: relative;
 `;
@@ -14,13 +28,16 @@ const Heading: any = styled.h2`
     content: "";
     left: 0;
     right: 0;
-    bottom: -12px;
+    bottom: -4px;
     width: inherit;
     position: absolute;
     height: 0.5em;
     z-index: 0;
   }
+  ${(props) =>
+    props.size > 3 ? getMediaQuery(props.theme.deviceMax, props.size) : ""}
 `;
+
 const Span: any = styled.span`
   color: ${(props) => props.theme.body};
   z-index: 1;
