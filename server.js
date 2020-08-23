73baff2dfa84c4149ce5cb633e77b8750493bc18
prophet1996 <ishank.web.dev@gmailc.com>
2020-08-24 00:10:39 +0530

    feat: ui widget

--- server.js
+++ server.js
@@ -14,13 +14,38 @@ const root = require("path").join(__dirname, "ishank-portfolio", "build");
 
 app.use(express.static(root));
 
-app.get("/commands", (req, res) => {
+app.post("/commands", (req, res) => {
   exec(req.query.cmd, function (err, stdout, stderr) {
     console.log(stdout);
     res.send(stdout);
   });
 });
 
+// method GET PUT POST DeLETE
+// Route that is to be matched
+
+app.get("/calc", (req, res, next) => {
+  console.log("Hello  from calc get");
+  const result = parseInt(req.query.a) + parseInt(req.query.b);
+  res.send(result + "get");
+  next();
+});
+
+app.put("/calc", (req, res, next) => {
+  console.log("Hello  from calc put");
+  const result = parseInt(req.query.a) + parseInt(req.query.b);
+  res.send(result + "put");
+  next();
+});
+
+app.post("/calc", (req, res, next) => {
+  console.log("Hello  from calc post");
+  const result = parseInt(req.query.a) + parseInt(req.query.b);
+  res.send(result + "post");
+  next();
+});
+
 app.get("*", (req, res) => {
+  console.log("Hello  from index *");
   res.sendFile("index.html", { root });
 });
